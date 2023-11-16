85a660f17027177c8ab67aa1d14011862a51212e
Wiktor Wolanin <volus7@o2.pl>
2023-11-17 00:09:10 +0100

    LogoutTimer&TimeOut

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,22 +1,21 @@
 const timeoutElement = document.getElementById('timeout');
 let lastActivity = Date.now();
 
-document.addEventListener('click', function () {
+
+document.addEventListener('click', () => {
     lastActivity = Date.now();
 });
 
 setInterval(function () {
     // Sprawdź, czy minęło więcej niż 5 minut od ostatniej aktywności
-    if (Date.now() - lastActivity > 5 * 60 * 1000) {
+    if (Date.now() - lastActivity > 5 * 60 * 1000 - 1000) {
         // Przekieruj użytkownika na stronę wylogowania
+        
         window.location.href = '/logout';
     }
 
     // Oblicz pozostały czas do wylogowania w sekundach
     const remainingTimeInSeconds = Math.ceil((5 * 60 * 1000 - 1000 - (Date.now() - lastActivity)) / 1000);
-
-    // Pobierz element div dla licznika czasu
-    const timeoutElement = document.getElementById('timeout');
 
     // Oblicz minutę i sekundy
     const minutes = Math.floor(remainingTimeInSeconds / 60);
