6cfb4d0b3125d73ce4cd93483b9c31f292766862
Wiktor Wolanin <volus7@o2.pl>
2023-11-17 00:12:36 +0100

    RearangingJS

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,6 +1,13 @@
 const timeoutElement = document.getElementById('timeout');
 let lastActivity = Date.now();
 
+let remainingTimeInSeconds = Math.ceil((5 * 60 * 1000 - 1000 - (Date.now() - lastActivity)) / 1000);
+
+    // Oblicz minutę i sekundy
+let minutes = Math.floor(remainingTimeInSeconds / 60);
+let seconds = remainingTimeInSeconds % 60;
+
+timeoutElement.innerHTML = `${minutes}:${seconds}`;
 
 document.addEventListener('click', () => {
     lastActivity = Date.now();
@@ -10,16 +17,14 @@ setInterval(function () {
     // Sprawdź, czy minęło więcej niż 5 minut od ostatniej aktywności
     if (Date.now() - lastActivity > 5 * 60 * 1000 - 1000) {
         // Przekieruj użytkownika na stronę wylogowania
-        
         window.location.href = '/logout';
     }
 
     // Oblicz pozostały czas do wylogowania w sekundach
-    const remainingTimeInSeconds = Math.ceil((5 * 60 * 1000 - 1000 - (Date.now() - lastActivity)) / 1000);
+    remainingTimeInSeconds = Math.ceil((5 * 60 * 1000 - 1000 - (Date.now() - lastActivity)) / 1000);
 
-    // Oblicz minutę i sekundy
-    const minutes = Math.floor(remainingTimeInSeconds / 60);
-    const seconds = remainingTimeInSeconds % 60;
+    minutes = Math.floor(remainingTimeInSeconds / 60);
+    seconds = remainingTimeInSeconds % 60;
 
     // Zaktualizuj treść diva z pozostałym czasem
     timeoutElement.innerHTML = `${minutes}:${seconds}`;
